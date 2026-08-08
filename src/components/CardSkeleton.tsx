9538cf684c2f51722bbbf8e32a600e2fb04d6866
Amedast <alexmedinaastorga@gmail.com>
2026-08-08 13:35:35 +0200

    Fix skeleton

--- src/components/CardSkeleton.tsx
+++ src/components/CardSkeleton.tsx
@@ -1,5 +1,3 @@
 export default function CardSkeleton() {
-  return (
-    <div className="aspect-63/88 rounded-xl skeleton-shimmer animate-pulse" />
-  );
+  return <div className="aspect-63/88 rounded-xl skeleton-pulse" />;
 }
